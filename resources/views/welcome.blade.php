86d0851f0d7b0f41de807e1430db657f130c4870
Vinicius Neiva <viniciustneiva@gmail.com>
2023-07-02 17:55:12 -0300

    Criação de upload simples

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,53 @@
     </head>
     <body>
     <main>
+        @if ($message = Session::get('success'))
+            <div id="alert-success-card" class="alert alert-success alert-block">
+                <button type="button" class="close btn btn-close" data-dismiss="alert"></button>
+                <strong>{{ $message }}</strong>
+            </div>
+        @endif
+
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <button type="button" class="close btn btn-close" data-dismiss="alert"></button>
+                <strong>Whoops!</strong> There were some problems with your input.
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <section class="container">
             <div class="row">
-                <div class="col-lg-12 text-center">
+                <div class="col-lg-12 text-center mt-4">
                     <h1>Estudo de caso Laravel FFMPEG</h1>
+                </div>
+            </div>
+            <div class="row my-5">
+                <div class="col-lg-12">
+                    <form action="{{ url('/upload') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="d-flex flex-column justify-content-between align-items-center">
+                        <input name="video" type="file" class="form-control form-control-lg" id="inputGroupFile" aria-describedby="inputGroupFileAddon" aria-label="Upload" accept="video/*">
+                    </div>
+                        <div class="col-md-6 form-group">
+                            <button type="submit" class="btn btn-success">Save</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </section>
     </main>
     </body>
+<script>
+    window.onload = function() {
+        document.querySelector('.close').addEventListener('click', function (){
+            document.querySelectorAll('.alert').forEach(item => {
+                item.style.display = 'none';
+            })
+        })
+    };
+</script>
 </html>
